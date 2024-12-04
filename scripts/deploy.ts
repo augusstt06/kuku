05f37323209daab0a88fcc8121eb1d37f1e5b699
augusstt06 <augusstt06@gmail.com>
2024-12-04 18:43:50 +0900

    add: basic deploy code

---
+++ scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  const KukuCoin = await ethers.getContractFactory("KukuCoin");
+  const kukuCoin = await KukuCoin.deploy(ethers.parseEther("1000000"));
+
+  console.log("KukuCoin deployed to:", kukuCoin.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
