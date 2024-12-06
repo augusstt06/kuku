a35f90274d49c5d061b45478b5704a297de7facb
augusstt06 <augusstt06@gmail.com>
2024-12-06 17:55:39 +0900

    fix: modify hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,17 @@ import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: [`0x${process.env.MAINNET_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
