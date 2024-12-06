7aed764091baba14ffee0ff9a0bada49deaea16b
augusstt06 <augusstt06@gmail.com>
2024-12-06 17:54:23 +0900

    fix: deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
+  // const [deployer] = await ethers.getSigners();
+  // console.log("Deploying contracts with the account:", deployer.address);
   const KukuCoin = await ethers.getContractFactory("KukuCoin");
   const kukuCoin = await KukuCoin.deploy();
-
   console.log("KukuCoin deployed to:", kukuCoin.address);
 }
 
