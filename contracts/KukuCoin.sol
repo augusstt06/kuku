9053db6a6b16d0c9db4f278b31d8c384e85ebb52
augusstt06 <augusstt06@gmail.com>
2024-12-06 15:50:55 +0900

    fix: modify constructor

--- contracts/KukuCoin.sol
+++ contracts/KukuCoin.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KukuCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("KukuCoin", "KUKU") {
-        _mint(msg.sender, initialSupply );
+contract KukuCoin is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("KukuCoin", "KUKU") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
