df18a5d8a9e2e3e9d6764a65f9f217c5a11f658c
augusstt06 <augusstt06@gmail.com>
2024-12-06 16:10:56 +0900

    add: airdrop

--- contracts/KukuCoin.sol
+++ contracts/KukuCoin.sol
@@ -12,6 +12,8 @@ contract KukuCoin is ERC20, Ownable {
     uint256 public immutable LIQUIDITY_SUPPLY;
     uint256 public immutable RESERVE_SUPPLY;
 
+    address[] public holders;
+
     constructor() ERC20("KukuCoin", "KUKU") Ownable(msg.sender) {
         TOTAL_SUPPLY = 100_000_000 * 10 ** decimals();
 
@@ -25,5 +27,26 @@ contract KukuCoin is ERC20, Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+    // airdrop 함수
+    function airdrop(uint256 minimumBalance, uint256 amount) external onlyOwner{
+        require(amount > 0, "Amount must be greater than 0");
+
+        for (uint256 i=0; i < holders.length;i++){
+            address holder = holders[i];
+            if (balanceOf(holder) >= minimumBalance){
+                _mint(holder, amount);
+            }
+        }
+    }
+    // holders 에 주소 추가
+    function addHolder(address holder) external {
+        require(holder != address(0), "Invalid address");
+        holders.push(holder);
+    }
+
+    // holders 조회
+    function getHolders() external view returns (address[] memory) {
+        return holders;
     }
 }
