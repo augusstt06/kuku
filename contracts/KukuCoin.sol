b26be505b72f3864bd320ee3df85504ec46003cb
augusstt06 <augusstt06@gmail.com>
2024-12-06 16:00:01 +0900

    fix: distribution ratio setting

--- contracts/KukuCoin.sol
+++ contracts/KukuCoin.sol
@@ -5,9 +5,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract KukuCoin is ERC20, Ownable {
-    uint public constant TOTAL_SUPPLY = 1000000 * 10 ** decimals();
-    constructor(uint256 initialSupply) ERC20("KukuCoin", "KUKU") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+    // 총 공급량을 1억 개로 설정
+    uint256 public immutable TOTAL_SUPPLY;
+
+    // 분배 비율
+    uint256 public immutable TEAM_SUPPLY;
+    uint256 public immutable COMMUNITY_SUPPLY;
+    uint256 public immutable LIQUIDITY_SUPPLY;
+    uint256 public immutable RESERVE_SUPPLY;
+
+    constructor() ERC20("KukuCoin", "KUKU") Ownable(msg.sender) {
+        TOTAL_SUPPLY = 100_000_000 * 10 ** decimals();
+
+        TEAM_SUPPLY = TOTAL_SUPPLY * 30 / 100;
+        COMMUNITY_SUPPLY = TOTAL_SUPPLY * 25 / 100; 
+        LIQUIDITY_SUPPLY = TOTAL_SUPPLY * 30 / 100; 
+        RESERVE_SUPPLY = TOTAL_SUPPLY * 15 / 100; 
+
+        _mint(msg.sender, TEAM_SUPPLY);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
