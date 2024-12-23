46976531c07d4ab0ff3ba8ab1fe8ca37db36e7ec
augusstt06 <augusstt06@gmail.com>
2024-12-23 22:54:43 +0900

    deploy: testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     hardhat: {},
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: `https://sepolia.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
     },
     // ropsten: {
     //   url: "https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID",
